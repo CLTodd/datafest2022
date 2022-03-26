f3a3a90b78430ede092aa9303f66ce72eef27232
twang53 <70387046+twang53@users.noreply.github.com>
2022-03-26 12:47:09 -0400

    Added merge

--- code/twang_sandbox.R
+++ code/twang_sandbox.R
@@ -6,8 +6,23 @@ library(data.table)
 library(dplyr)
 library(tidyr)
 
+
 # logs data
-logs <- read.csv("C:\Users\GuaiGuai\Downloads\datafest2022-main\data\logs.csv")
+# 12 diff schools
+# 166 diff players
+logs <- read.csv("C:/Users/GuaiGuai/Downloads/datafest2022-main/data/logs.csv")
+
+# need to re-do, this is pointless
+school <- logs %>%
+  group_by(school) %>%
+  select(player_id, school, session, event_category, event_time_dbl, avatar_gender, avatar_id, event_type, skill_level_know)
+
 
 # S5_scores_cleaned data
-s5_Scores <- read.csv("C:\Users\GuaiGuai\Downloads\datafest2022-main\data\S5_scores_cleaned.csv")
+# range: 1.3-4
+# 64 diff players
+s5_Scores <- read.csv("C:/Users/GuaiGuai/Downloads/datafest2022-main/data/S5_scores_cleaned.csv")
+
+
+# merge(s5 scores, logs by playerid)
+combo <- merge(school, s5_Scores, by="player_id")
