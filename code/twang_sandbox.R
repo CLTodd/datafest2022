2db467ed72f70e0dc0825e181f2dbd35fa5ed76b
twang53 <70387046+twang53@users.noreply.github.com>
2022-03-26 11:39:58 -0400

    Tanya's sandbox added

---
+++ code/twang_sandbox.R
@@ -0,0 +1,13 @@
+# Tanya's Sandbox
+# 3/26/2022
+
+# import packages
+library(data.table)
+library(dplyr)
+library(tidyr)
+
+# logs data
+logs <- read.csv("C:\Users\GuaiGuai\Downloads\datafest2022-main\data\logs.csv")
+
+# S5_scores_cleaned data
+s5_Scores <- read.csv("C:\Users\GuaiGuai\Downloads\datafest2022-main\data\S5_scores_cleaned.csv")
