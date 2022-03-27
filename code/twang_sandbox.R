e256e9ad48c179b602eec273c7fed75ecf473f2e
twang53 <70387046+twang53@users.noreply.github.com>
2022-03-26 22:17:10 -0400

    Updated code w/ more params

--- code/twang_sandbox.R
+++ code/twang_sandbox.R
@@ -5,6 +5,8 @@
 library(data.table)
 library(dplyr)
 library(tidyr)
+library("FastImputation")
+library(missRanger)
 
 
 # logs data
@@ -12,24 +14,81 @@ library(tidyr)
 # 166 diff players
 logs <- read.csv("C:/Users/GuaiGuai/Downloads/datafest2022-main/data/logs.csv")
 
+# S5_scores_cleaned data
+# range: 1.3-4
+# 64 diff players
+s5_Scores <- read.csv("C:/Users/GuaiGuai/Downloads/datafest2022-main/data/S5_scores_cleaned.csv")
+
+###########################################################################################################################
+
+# explore data types
+str(logs)
+
+# create sub dataframe w/ only numeric vars of interest
+sub <- logs %>% 
+  select(event_id, event_time_dbl, stack_id, minigame_level, skill_level_know, skill_level_priority, 
+              skill_level_people, skill_level_refusal, skill_level_me, avatar_age, 
+         school, minigame_id, old_skill_point, new_skill_point, aa_level_id, chunk_id, piece_id, 
+         old_choice_id, choice_id, new_choice_id, total_points, total_strikes)
+
+# create numeric var for gender
+sub$avatar_gender <- ifelse(logs$avatar_gender == 'Male', 1, 0)
+
+# check for correlation
+cor(sub)
+
+## NOTES:
+
+# pos correlations: 
+## event_time_dbl & event_id > event_time_dbl & school
+
+# neg correlations: 
+## event_id & school
+
+# final dataframe (consider adding "date" var?)
+final <- logs %>%
+  select(player_id, event_id, event_time_dbl, stack_id, minigame_level, skill_level_know, skill_level_priority, 
+         skill_level_people, skill_level_refusal, skill_level_me, avatar_age, 
+         school, minigame_id, old_skill_point, new_skill_point, aa_level_id, chunk_id, piece_id, 
+         old_choice_id, choice_id, new_choice_id, total_points, total_strikes)
+final$avatar_gender <- ifelse(logs$avatar_gender == 'Male', 1, 0)
+final$avatar_id <- unclass(factor(logs$avatar_id))
+final$session <- unclass(factor(logs$session))
+final$skill_id <- unclass(factor(logs$skill_id))
+
+# merge log subset & s5_Scores
+combo <- merge(final, s5_Scores, by="player_id")
+
+temp <- missRanger(data=combo, 
+                   seed=16802, 
+                   maxiter=5, 
+                   verbose=1)
+
+mod1 <- lm(S5_mean ~., data=combo)
+summary(mod1)
+
+
+###########################################################################################################################
+
 # need to re-do, this is pointless
 school <- logs %>%
   group_by(school) %>%
   select(player_id, school, event_id, session, event_category, event_time_dbl, avatar_gender, avatar_id, event_type, skill_level_know)
 
 
-# S5_scores_cleaned data
-# range: 1.3-4
-# 64 diff players
-s5_Scores <- read.csv("C:/Users/GuaiGuai/Downloads/datafest2022-main/data/S5_scores_cleaned.csv")
-
-
 # merge log subset & s5_Scores
 combo <- merge(school, s5_Scores, by="player_id")
 
-# model
+# simple model
 model1 <- lm(S5_mean ~ event_time_dbl + weeks, data=combo)
 summary(model1)
+
+# more complex
+model2 <- lm(S5_mean ~ player_id + session + event_category + event_time_dbl + weeks, data=combo)
+summary(model2)
+
+# correlation matrix
+# cor(insert dataframe here)
 
 # convert s5 to wide format
 s5_wide <- 
@@ -40,9 +99,11 @@ s5_wide <-
                      names_prefix = "week")
 
 # convert school to wide format
-school_wide <- 
-  tidyr::pivot_wider(school[school$player_id != "NA",], 
-                     id_cols = player_id, 
-                     names_from = event_id, 
-                     values_from = event_time_dbl, 
-                     names_prefix = "event")
+#school_wide <- 
+#  tidyr::pivot_wider(school[school$player_id != "NA",], 
+#                     id_cols = player_id, 
+#                     names_from = session, 
+#                     values_from = event_time_dbl, 
+#                     names_prefix = "event")
+
+###########################################################################################################################
